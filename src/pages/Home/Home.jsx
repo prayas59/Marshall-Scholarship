95a245e2588508c1e9ba0a2a6ae885df98840bd3
Prayas Godara <godara.prayas23@gmail.com>
2024-01-23 02:26:45 +0530

    updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,34 +2,6 @@ import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 
-import Blog1Img from "../../utils/images/blog1-img.jpg";
-import Blog2Img from "../../utils/images/blog2-img.jpg";
-import Blog3Img from "../../utils/images/blog3-img.jpg";
-
-const blogs = [
-  {
-    id: 1,
-    img: [Blog1Img],
-    title: "Blog 1",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.",
-  },
-  {
-    id: 2,
-    img: [Blog2Img],
-    title: "Blog 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.",
-  },
-  {
-    id: 3,
-    img: [Blog3Img],
-    title: "Blog 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.",
-  },
-];
-
 function Home() {
   return (
     <div className="home-page">
--- src/pages/Scholarships/Courses.jsx
+++ src/pages/Scholarships/Courses.jsx
@@ -1,73 +1,7 @@
 import React from "react";
 import "./Courses.css";
-import ArtCourseImg from "../../utils/images/art-course.jpg";
-import BusinessCourseImg from "../../utils/images/business-course.jpg";
-import ComputerScienceCourseImg from "../../utils/images/computer-science-course.jpg";
-import EducationCourseImg from "../../utils/images/education-course.jpg";
-import HealthcareCourseImg from "../../utils/images/healthcare-course.jpg";
-import LawCourseImg from "../../utils/images/law-course.jpg";
-import MusicCourseImg from "../../utils/images/music-course.jpg";
-import SportCourseImg from "../../utils/images/sport-course.jpg";
-import ScholarshipForm from "../../components/ScholarshipForm";
 
-const courses = [
-  {
-    id: 1,
-    img: [ArtCourseImg],
-    title: "Art Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 2,
-    img: [BusinessCourseImg],
-    title: "Business Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 3,
-    img: [ComputerScienceCourseImg],
-    title: "Computer Science Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 4,
-    img: [EducationCourseImg],
-    title: "Education Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 5,
-    img: [HealthcareCourseImg],
-    title: "Healthcare Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 6,
-    img: [LawCourseImg],
-    title: "Law Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 7,
-    img: [MusicCourseImg],
-    title: "Music Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-  {
-    id: 8,
-    img: [SportCourseImg],
-    title: "Sport Course",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
-  },
-];
+import ScholarshipForm from "../../components/ScholarshipForm";
 
 function Courses() {
   return (
