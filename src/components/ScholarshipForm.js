eb5412a78e5a1a5460aac95f42ddc80296212b37
Prayas Godara <godara.prayas23@gmail.com>
2024-02-05 03:45:55 +0530

    req

--- src/components/ScholarshipForm.js
+++ src/components/ScholarshipForm.js
@@ -111,7 +111,6 @@ const FormComponent = () => {
                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                   onChange={(option) => setFieldValue("university", option)}
                   value={values.university}
-                  required
                 />
               </div>
               <button
