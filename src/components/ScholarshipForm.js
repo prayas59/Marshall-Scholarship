7d0c49c8fe7c7f6c8f92278a8bd1caff344bb960
Prayas Godara <godara.prayas23@gmail.com>
2024-02-05 15:03:00 +0530

    UI

--- src/components/ScholarshipForm.js
+++ src/components/ScholarshipForm.js
@@ -4,7 +4,6 @@ import "tailwindcss/tailwind.css"; // Import Tailwind CSS
 
 const FormComponent = () => {
   const [universities, setUniversities] = useState([]);
-  const [error, setError] = useState(null);
   const [selectedUniversity, setSelectedUniversity] = useState(null);
 
   useEffect(() => {
@@ -24,85 +23,117 @@ const FormComponent = () => {
         setUniversities(universityOptions);
       })
       .catch((error) => {
-        setError(error.message);
         console.error("Error fetching universities:", error);
       });
   }, []);
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-xl">
-        <h2 className="text-2xl mb-4 text-center font-bold">Submit Form</h2>
-        {error && <div className="text-red-500">{error}</div>}
-
+    <div>
+      <link rel="stylesheet" href="https://cdn.formbold.com/formbold.css" />
+      <br />
+      <div className=" formbold-builder-form themeOne ">
         <form
           name="contact"
+          enctype="multipart/form-data"
           method="POST"
-          data-netlify="true"
+          className="mx-auto w-full max-w-[570px] rounded-[10px] border border-stroke bg-white div-10 themeOne"
+          action="https://formbold.com/s/6lq7n"
           onSubmit="submit"
         >
-          <p>
-            <label htmlFor="firstName" className="block text-gray-700">
-              First Name:
-            </label>
-            <input
-              type="text"
-              name="firstName"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="John"
-              required
-            />
-          </p>
-          <p>
-            <label htmlFor="lastName" className="block text-gray-700">
-              Last Name:
-            </label>
-            <input
-              type="text"
-              name="lastName"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="Roy"
-              required
-            />
-          </p>
-          <p>
-            <label htmlFor="phoneNumber" className="block text-gray-700">
-              Phone Number:
-            </label>
-            <input
-              type="text"
-              name="phoneNumber"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="+44 20 7123 4567"
-              required
-            />
-          </p>
-          <p>
-            <label htmlFor="email" className="block text-gray-700">
-              Email:
-            </label>
-            <input
-              type="email"
-              name="email"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="johnroy@gmail.com"
-              required
-            />
-          </p>
-          <p>
-            <label htmlFor="university" className="block text-gray-700">
-              University:
-            </label>
-            <Select
-              options={universities}
-              className="mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              onChange={setSelectedUniversity}
-              value={selectedUniversity}
-            />
-          </p>
+          <div className="SortableItem fb-builder-item">
+            <div className="">
+              <label
+                htmlFor="firstName"
+                className="mb-2.5 block text-base text-black"
+              >
+                <span>First Name:</span>
+                <span class="label-required pl-1 text-red-400">*</span>
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                className="w-full rounded border border-stroke bg-white px-5 py-3 text-base text-black outline-none focus:border-primary"
+                placeholder="John"
+                required
+              />
+            </div>
+          </div>
+          <div className="SortableItem fb-builder-item">
+            <div className="">
+              <label
+                htmlFor="lastName"
+                className="mb-2.5 block text-base text-black"
+              >
+                <span>Last Name:</span>
+                <span class="label-required pl-1 text-red-400">*</span>
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                className="w-full rounded border border-stroke bg-white px-5 py-3 text-base text-black outline-none focus:border-primary"
+                placeholder="Roy"
+                required
+              />
+            </div>
+          </div>
+          <div className="SortableItem fb-builder-item">
+            <div className="">
+              <label
+                htmlFor="phoneNumber"
+                className="mb-2.5 block text-base text-black"
+              >
+                <span>Phone Number:</span>
+                <span class="label-required pl-1 text-red-400">*</span>
+              </label>
+              <input
+                type="text"
+                name="phoneNumber"
+                className="w-full rounded border border-stroke bg-white px-5 py-3 text-base text-black outline-none focus:border-primary"
+                placeholder="+44 20 7123 4567"
+                required
+              />
+            </div>
+          </div>
+          <div className="SortableItem fb-builder-item">
+            <div className="">
+              <label
+                htmlFor="email"
+                className="mb-2.5 block text-base text-black"
+              >
+                <span>Email Id:</span>
+                <span class="label-required pl-1 text-red-400">*</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                className="w-full rounded border border-stroke bg-white px-5 py-3 text-base text-black outline-none focus:border-primary"
+                placeholder="johnroy@gmail.com"
+                required
+              />
+            </div>
+          </div>
+          <div className="SortableItem fb-builder-item">
+            <div className="">
+              <label
+                htmlFor="university"
+                className="mb-2.5 block text-base text-black"
+              >
+                <span>University</span>
+                <span class="label-required pl-1 text-red-400">*</span>
+              </label>
+              <Select
+                options={universities}
+                name="univ"
+                className="w-full rounded border border-stroke bg-white px-5 py-3 text-base text-black outline-none focus:border-primary"
+                onChange={setSelectedUniversity}
+                value={selectedUniversity}
+              />
+            </div>
+          </div>
+
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            className="inline-flex cursor-pointer items-center justify-center rounded border border-primary bg-primary px-8 py-2 text-base font-medium text-white hover:bg-opacity-90"
           >
             Submit
           </button>
