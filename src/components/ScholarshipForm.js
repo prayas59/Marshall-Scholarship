50b4687ff0a6e4bb98650f560b75db7d8aa87948
Prayas Godara <godara.prayas23@gmail.com>
2024-02-05 14:35:53 +0530

    submit

--- src/components/ScholarshipForm.js
+++ src/components/ScholarshipForm.js
@@ -35,7 +35,12 @@ const FormComponent = () => {
         <h2 className="text-2xl mb-4 text-center font-bold">Submit Form</h2>
         {error && <div className="text-red-500">{error}</div>}
 
-        <form name="contact" method="POST" data-netlify="true">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit="submit"
+        >
           <p>
             <label htmlFor="firstName" className="block text-gray-700">
               First Name:
